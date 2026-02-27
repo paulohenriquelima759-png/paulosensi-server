761aa6a402c471855488cb905cd963ef82030cd1
paulohenriquelima759-png <paulohenriquelima759@gmail.com>
2026-02-27 17:33:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -5,16 +6,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Servidor do Paulo Sensi está ONLINE 🚀");
-});
+const WEB_DIR = path.join(__dirname, "web");
+app.use(express.static(WEB_DIR));
 
 app.get("/api/ping", (req, res) => {
   res.json({ ok: true });
 });
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(WEB_DIR, "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
